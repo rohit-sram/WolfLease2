31bf35f75194db6c5381919301896280acb53b6a
Yash Bhansali <bhansaliyashg@gmail.com>
2022-12-02 01:41:35 -0500

    update ts for home-page

--- angular/src/app/features/app-shell/containers/home-page/home-page.component.ts
+++ angular/src/app/features/app-shell/containers/home-page/home-page.component.ts
@@ -20,5 +20,28 @@ export class HomePageComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  goToFeature(type: string) {
+   let data = [
+    {
+        "id": "01f63923-61f9-4d33-969f-c0b4be7ebb14",
+        "address": "2343",
+        "facilities": "Utilities Included",
+        "owner_id": "e1e1253b-264c-4589-9dcc-2d411acc446f"
+    }
+]
+
+
+    if(type == 'owners'){
+      this.simpleModalService.addModal(OwnerModalComponent,{type: type,data: data});
+    } else if (type == 'flats'){
+      this.simpleModalService.addModal(FlatModalComponent,{type: type, data: data});
+    } else if (type == 'apartments'){
+      this.simpleModalService.addModal(ApartmentModalComponent,{type: type, data: data})
+    } else if (type == 'users'){
+      this.simpleModalService.addModal(UserModalComponent,{type: type, data: data})
+    } else if (type == 'lease'){
+      this.simpleModalService.addModal(LeaseModalComponent,{type: type, data: data})
+    }
+  }
 
 }
