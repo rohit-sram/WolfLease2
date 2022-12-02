6a4806562b7147e68a770f678bdbca84da7336ba
Yash Bhansali <bhansaliyashg@gmail.com>
2022-12-02 01:53:49 -0500

    add ts for user

---
+++ angular/src/app/features/app-shell/containers/user-modal/user-modal.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { SimpleModalComponent, SimpleModalService } from 'ngx-simple-modal';
+import { FeatureService } from '../../services/feature.service';
+import { RenderDataComponent } from '../render-data/render-data.compnent';
+import { AddEditUser } from '../add-edit-user/add-edit-user.component';
+
+@Component({
+  selector: 'app-user-modal',
+  templateUrl: './user-modal.component.html'
+})
+export class UserModalComponent extends SimpleModalComponent<{type: string; data: any},boolean> implements OnInit {
+
+  type!: string;
+  data!: any;
+
+  constructor(private readonly featureService: FeatureService,
+            private readonly modalService: SimpleModalService) {
+    super()
+  }
+}
