b02a9e8145ec9447df87d2811fb65533a3fed3a8
Manogna Choudary Potluri <manognacpotluri@gmail.com>
2022-12-02 00:27:35 -0500

    update settings to include token and authentication details

--- wolf_lease/settings.py
+++ wolf_lease/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'housing',
     'authentication',
 ]
@@ -74,10 +75,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'wolf_lease.wsgi.application'
 
 REST_FRAMEWORK = {
-    # 'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
-    # 'DEFAULT_AUTHENTICATION_CLASSES': [
-    #     'rest_framework_simplejwt.authentication.JWTAuthentication',
-    # ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.TokenAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
