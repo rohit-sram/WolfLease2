0bc64a4d0b784c52f05365a8645178ab7777d011
Priyam Garg <gargpriyam21@gmail.com>
2022-12-02 02:12:48 -0500

    add module for app-shell

---
+++ angular/src/app/features/app-shell/app-shell.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppShellComponent } from './containers/app-shell/app-shell.component';
+import { HomePageComponent } from './containers/home-page/home-page.component';
+import {appShellRoutes} from './app-shell.routes';
+import { FlatModalComponent } from './containers/flat-modal/flat-modal.component';
+import { FeatureService } from './services/feature.service';
+import {defaultSimpleModalOptions, SimpleModalModule} from 'ngx-simple-modal';
+import { CommonModule, TitleCasePipe } from '@angular/common';
+import { RenderDataComponent } from './containers/render-data/render-data.compnent';
+import { AddEditApartment } from './containers/add-edit-apartment/add-edit-apartment.component';
+import { AddEditFlat } from './containers/add-edit-flat/add-edit-flat.component';
+import { AddEditLease } from './containers/add-edit-lease/add-edit-lease.component';
+import { AddEditOwner } from './containers/add-edit-owner/add-edit-owner.component';
+import { AddEditUser } from './containers/add-edit-user/add-edit-user.component';
+import { UserModalComponent } from './containers/user-modal/user-modal.component';
+import { OwnerModalComponent } from './containers/owner-modal/owner-modal.component';
+import { LeaseModalComponent } from './containers/lease-modal/lease-modal.component';
+import { ApartmentModalComponent } from './containers/apartment-modal/apartment-modal.component';
+import { BrowserModule } from '@angular/platform-browser';
+
+const routes: Routes = [
+  {path: '',component: AppShellComponent }
+];
+
+export class AppShellModule { }
