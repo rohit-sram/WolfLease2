ac4349772ec06d0b914da18c472976fc839f5136
Divyang Doshi <divyangdoshi02@gmail.com>
2022-12-02 02:14:48 -0500

    add routing for app-shell

---
+++ angular/src/app/features/app-shell/app-shell.routes.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AppShellComponent } from './containers/app-shell/app-shell.component';
+import { HomePageComponent } from './containers/home-page/home-page.component';
+
+export const appShellRoutes: Routes = [
+  {
+    path: '',
+    component: AppShellComponent,
+  }
+ ];
