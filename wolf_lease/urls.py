e9f29ddfe063bd5e1a86640ed8496efd05c22e1d
Manogna Choudary Potluri <manognacpotluri@gmail.com>
2022-12-02 00:07:54 -0500

    add authentication settings

--- wolf_lease/urls.py
+++ wolf_lease/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('housing.urls'))
+    path('', include('housing.urls')),
+    path('', include('authentication.urls')),
 ]
