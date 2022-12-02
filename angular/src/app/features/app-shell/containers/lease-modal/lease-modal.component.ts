15332a5a1934bec941e55075544bd43042f50443
Manogna Choudary Potluri <manognacpotluri@gmail.com>
2022-12-02 15:56:50 -0500

    update ts for lease

--- angular/src/app/features/app-shell/containers/lease-modal/lease-modal.component.ts
+++ angular/src/app/features/app-shell/containers/lease-modal/lease-modal.component.ts
@@ -25,16 +25,29 @@ export class LeaseModalComponent extends SimpleModalComponent<{type: string; dat
   search(event: any){
     this.featureService.searchFeature(this.type,this.searchvalue)
     .subscribe((searchdata: any) => {
-      this.modalService.addModal(RenderDataComponent,{type: this.type,data: searchdata});
+      this.modalService.addModal(RenderDataComponent,{type: this.type,data: searchdata})
+      .subscribe((result: any) => {
+        this.updateData();
+      });
     })
   }
 
   addFeature(){
       this.modalService.addModal(AddEditLease, {type: 'add', data: {}})
+      .subscribe((success: any) => {
+          this.updateData();
+      })
   }
 
   onSearch(event: any){
     this.searchvalue = event.target.value;
+  }
+
+  updateData(){
+    this.featureService.getFeature(this.type)
+    .subscribe((getdata: any) => {
+      this.data = getdata;
+    })
   }
 
 
