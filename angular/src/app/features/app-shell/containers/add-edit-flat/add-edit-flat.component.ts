27e67a6bde8830d36ab71d6d7a593d44be1540c7
Manogna Choudary Potluri <manognacpotluri@gmail.com>
2022-12-02 01:16:51 -0500

    add ts file for flat add/edit

---
+++ angular/src/app/features/app-shell/containers/add-edit-flat/add-edit-flat.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { SimpleModalComponent } from 'ngx-simple-modal';
+import { FeatureService } from '../../services/feature.service';
+
+@Component({
+  selector: 'app-add-edit-flat',
+  templateUrl: './add-edit-flat.component.html'
+})
+export class AddEditFlat extends SimpleModalComponent<{type: string; data: any},boolean> implements OnInit {
+
+  type!: string;
+  data!: any;
+
+  availability!: boolean;
+  apartment: any = '';
+  lease: any = '';
+  rent: any = '';
+  floor_number: any = '';
+
+  constructor(private readonly featureService: FeatureService) {
+    super()
+  }
+
+  ngOnInit(): void {
+    if(this.type == 'edit'){
+      this.availability = this.data.availability;
+      this.apartment = this.data.associated_apt_id;
+      this.lease = this.data.lease_id;
+      this.rent = this.data.rent_per_room;
+      this.floor_number = this.data.floor_number;
+    }
+  }
+
+
+}
