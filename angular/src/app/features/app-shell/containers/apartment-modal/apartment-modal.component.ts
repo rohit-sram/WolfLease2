58c8570b57eff13ff43812446eb352e5f1683d47
Divyang Doshi <divyangdoshi02@gmail.com>
2022-12-02 15:55:40 -0500

    update ts for apartment

--- angular/src/app/features/app-shell/containers/apartment-modal/apartment-modal.component.ts
+++ angular/src/app/features/app-shell/containers/apartment-modal/apartment-modal.component.ts
@@ -25,16 +25,28 @@ export class ApartmentModalComponent extends SimpleModalComponent<{type: string;
   search(event: any){
     this.featureService.searchFeature(this.type,this.searchvalue)
     .subscribe((searchdata: any) => {
-      this.modalService.addModal(RenderDataComponent,{type: this.type,data: searchdata});
+      this.modalService.addModal(RenderDataComponent,{type: this.type,data: searchdata})
+      .subscribe((result: any) => {
+        this.updateData();
+      });
     })
   }
   
   addFeature(){
       this.modalService.addModal(AddEditApartment,{type: 'add', data: {}})
+      .subscribe((success: any) => {
+          this.updateData();
+      })
   }
   onSearch(event: any){
     this.searchvalue = event.target.value;
   }
 
+  updateData(){
+    this.featureService.getFeature(this.type)
+    .subscribe((getdata: any) => {
+      this.data = getdata;
+    })
+  }
 
 }
