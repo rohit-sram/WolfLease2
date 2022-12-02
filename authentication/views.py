ea75d0c6d39f219751a3337ec1b9f5d668475c89
Priyam Garg <gargpriyam21@gmail.com>
2022-12-02 00:21:15 -0500

    add login view

--- authentication/views.py
+++ authentication/views.py
@@ -1,9 +1,27 @@
-from django.contrib.auth.models import User
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 from rest_framework import generics
-from rest_framework import permissions
+from rest_framework.authtoken.models import Token
+from rest_framework import permissions, views
+from rest_framework.response import Response
+from django.contrib.auth import login
+
 
 
 class RegisterView(generics.CreateAPIView):
     permission_classes = (permissions.AllowAny,)
     serializer_class = RegisterSerializer
+
+class LoginView(views.APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        serializer = LoginSerializer(data=self.request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        
+        token,_ = Token.objects.get_or_create(user=user)
+        token = str(token)
+
+        login(request, user)
+        content = {'message': 'Login successfull', 'token' : token}
+        return Response(content)
