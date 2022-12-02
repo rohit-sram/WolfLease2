741e9ae43af97a5900ddd32fb211f4fc8596065f
Priyam Garg <gargpriyam21@gmail.com>
2022-12-02 01:51:57 -0500

    update ts for render-data

--- angular/src/app/features/app-shell/containers/render-data/render-data.compnent.ts
+++ angular/src/app/features/app-shell/containers/render-data/render-data.compnent.ts
@@ -25,4 +25,21 @@ export class RenderDataComponent extends SimpleModalComponent<{type: string; dat
   ngOnInit(): void {
   }
 
+  updateFeature(){
+    if(this.type=='owners'){
+        this.modalService.addModal(AddEditOwner,{type: 'edit',data: this.data[0]})
+      } else if(this.type == 'flats'){
+        this.modalService.addModal(AddEditFlat,{type: 'edit', data: this.data[0]})
+      } else if(this.type == 'apartments'){
+        this.modalService.addModal(AddEditApartment,{type: 'edit', data: this.data[0]})
+      } else if(this.type == 'users'){
+        this.modalService.addModal(AddEditUser,{type:'edit',data: this.data[0] })
+      } else if(this.type == 'lease'){
+        this.modalService.addModal(AddEditLease, {type: 'edit', data: this.data[0]})
+      }
+  }
+
+  deleteFeature(){
+
+  }
 }
