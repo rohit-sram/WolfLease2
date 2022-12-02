14f9314b57d83855c481c6ec3e5463d7bd31d519
Dev Mehta <devmehta5799@gmail.com>
2022-12-02 00:23:57 -0500

    add logout url

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from authentication.views import RegisterView, LoginView
+from authentication.views import RegisterView, LoginView, LogoutView
 
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='auth_register'),
     path('login/', LoginView.as_view(), name='auth_login'),
+    path('logout/', LogoutView.as_view(), name='auth_logout'),
 ]
