63fd9ab926dd152e75a46016c89bd47a01c3d80c
Divyang Doshi <divyangdoshi02@gmail.com>
2022-12-02 00:17:38 -0500

    add validate and create methods for registration serializer

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -15,3 +15,30 @@ class RegisterSerializer(serializers.ModelSerializer):
 
     password = serializers.CharField(write_only=True, required=True)
     password2 = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+    def validate(self, validated_data):
+        if validated_data['password'] != validated_data['password2']:
+            raise serializers.ValidationError({"error": "Password fields didn't match."})
+
+        return validated_data
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+        
+        return user
