90bd0c2168f1d5281e11210ad14428009c519854
Divyang Doshi <divyangdoshi02@gmail.com>
2022-12-02 01:34:55 -0500

    add ts for apartment

---
+++ angular/src/app/features/app-shell/containers/apartment-modal/apartment-modal.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { SimpleModalComponent, SimpleModalService } from 'ngx-simple-modal';
+import { FeatureService } from '../../services/feature.service';
+import { RenderDataComponent } from '../render-data/render-data.compnent';
+import { AddEditApartment } from '../add-edit-apartment/add-edit-apartment.component';
+
+@Component({
+  selector: 'app-apartment-modal',
+  templateUrl: './apartment-modal.component.html'
+})
+export class ApartmentModalComponent extends SimpleModalComponent<{type: string; data: any},boolean> implements OnInit {
+
+  type!: string;
+  data!: any;
+
+  constructor(private readonly featureService: FeatureService,
+            private readonly modalService: SimpleModalService) {
+    super()
+  }
+
+}
