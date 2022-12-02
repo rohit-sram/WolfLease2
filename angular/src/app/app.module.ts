071eea6f62b0b9dd4b524b73c756cf74ae95d8df
Divyang Doshi <divyangdoshi02@gmail.com>
2022-12-02 02:09:47 -0500

    add module file for app

---
+++ angular/src/app/app.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router'
+import { appRoutes } from './app.routes'
+
+import { AppComponent } from './app.component';
+
+export class AppModule { }
