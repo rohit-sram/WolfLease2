882f4b8aa050242872e6eef4c86f66084c5fc710
Dev Mehta <devmehta5799@gmail.com>
2022-12-02 15:58:35 -0500

    update ts for user

--- angular/src/app/features/app-shell/containers/user-modal/user-modal.component.ts
+++ angular/src/app/features/app-shell/containers/user-modal/user-modal.component.ts
@@ -25,16 +25,29 @@ export class UserModalComponent extends SimpleModalComponent<{type: string; data
   search(event: any){
     this.featureService.searchFeature(this.type,this.searchvalue)
     .subscribe((searchdata: any) => {
-      this.modalService.addModal(RenderDataComponent,{type: this.type,data: searchdata});
+      this.modalService.addModal(RenderDataComponent,{type: this.type,data: searchdata})
+      .subscribe((result: any) => {
+        this.updateData();
+      });
     })
   }
 
   addFeature(){
     this.modalService.addModal(AddEditUser,{type:'add', data: {} })
+    .subscribe((success: any) => {
+        this.updateData();
+    })
   }
 
   onSearch(event: any){
     this.searchvalue = event.target.value;
+  }
+
+  updateData(){
+    this.featureService.getFeature(this.type)
+    .subscribe((getdata: any) => {
+      this.data = getdata;
+    })
   }
 
 
