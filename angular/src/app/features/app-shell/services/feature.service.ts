494b46a00133846306a92459ae199415cb999409
Dev Mehta <devmehta5799@gmail.com>
2022-12-02 23:20:04 -0500

    update service file for feature

--- angular/src/app/features/app-shell/services/feature.service.ts
+++ angular/src/app/features/app-shell/services/feature.service.ts
@@ -10,26 +10,53 @@ export class FeatureService {
   constructor(private readonly httpClient: HttpClient) { }
 
   getFeature(type: string): any{
-    return this.httpClient.get<any>(appShellEndpoints.getPostSearchFeature(type));
+    return this.httpClient.get<any>(appShellEndpoints.getPostSearchFeature(type),{
+      headers: {
+        Authorization: 'Token '+localStorage.getItem('token')
+      }
+    });
   }
 
   searchFeature(type: string,search: string): any{
     return this.httpClient.get<any>(appShellEndpoints.getPostSearchFeature(type), {
       params: {
         search
+      },
+      headers: {
+        Authorization: 'Token '+localStorage.getItem('token')
       }
     });
   }
 
   deleteFeature(type: string,id: any){
-    return this.httpClient.delete(appShellEndpoints.putDeleteFeature(type,id));
+    return this.httpClient.delete(appShellEndpoints.putDeleteFeature(type,id),{
+      headers: {
+        Authorization: 'Token '+localStorage.getItem('token')
+      }
+    });
   }
 
   putFeature(type: string,id: any, data: any){
-    return this.httpClient.put<any>(appShellEndpoints.putDeleteFeature(type,id),data);
+    return this.httpClient.put<any>(appShellEndpoints.putDeleteFeature(type,id),data,{
+      headers: {
+        Authorization: 'Token '+localStorage.getItem('token')
+      }
+    });
   }
 
   postFeature(type: string, data: any){
-    return this.httpClient.post<any>(appShellEndpoints.getPostSearchFeature(type),data);
+    return this.httpClient.post<any>(appShellEndpoints.getPostSearchFeature(type),data,{
+      headers: {
+        Authorization: 'Token '+localStorage.getItem('token')
+      }
+    });
+  }
+
+  logout(){
+    return this.httpClient.get<any>('http://localhost:8000/logout',{
+      headers: {
+        Authorization: 'Token '+localStorage.getItem('token')
+      }
+    })
   }
 }
