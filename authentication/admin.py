330d4a18cf9b7a5a9b376c7677de8722a8ffc7cd
Yash Bhansali <bhansaliyashg@gmail.com>
2022-12-04 03:11:47 -0500

    Adding documentation for authentication

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
+"""
+Authentication admin
+"""
 # Register your models here.
--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,6 @@
 from django.test import TestCase
 
+"""
+Authentication tests
+"""
 # Create your tests here.
