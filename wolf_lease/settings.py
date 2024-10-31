da81fb80e2392db885fe4bad3fde3cbcf436fc44
Jap Purohit <pjap041@gmail.com>
2024-10-31 01:35:05 -0400

    Added the configuration for the AWS RDS Instance with Django

--- wolf_lease/settings.py
+++ wolf_lease/settings.py
@@ -84,8 +84,17 @@ REST_FRAMEWORK = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wolflease',
+        'USER': 'admin',
+        'PASSWORD': 'dummy',
+        'HOST': 'dummy',
+        'PORT': 3306,
+        'OPTIONS': {
+            'ssl': {
+                'ca': './us-east-2-bundle.pem'
+            }
+        }
     }
 }
 
